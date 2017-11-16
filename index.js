825b0336d4cb2e530a0a49e6d96c0a5c8f83a154
futpib <futpib@gmail.com>
2017-11-16 21:55:06 +0300

    Add the rules

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	rules: {
+		'array-bracket-spacing': ['error', 'always'],
+		'object-curly-spacing': ['error', 'always'],
+
+		'comma-dangle': ['error', 'always-multiline']
+	}
+};
